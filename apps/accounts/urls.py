b143c57dba25f63952a9c1cabade3e70975757d1
DESKTOP-R889SGE\aws9 <b>
2025-07-23 11:40:20 +0900

    login 기능

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = 'accounts'
 
+router = DefaultRouter()
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('login/', views.login_view, name='login'), 
     path('logout/', views.logout_view, name='logout'),
+    path('user/', views.user_info_view, name='user_info'),
 ]
